ac074318793605865d4c5f44ac0aba2102efd158
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-25 09:48:37 -0500

    ex19 nested select madness

---
+++ top_2009_agent.sql
@@ -0,0 +1,11 @@
+SELECT
+    EmployeeName,
+    Max(TotalSales) as MaxSales
+FROM (select sum(i.total) as TotalSales,
+        e.FirstName || " " || e.LastName as EmployeeName
+        FROM employee e
+        JOIN Customer c on c.supportrepid = e.EmployeeId
+        JOIN Invoice i on i.customerid = c.customerid
+        WHERE i.InvoiceDate LIKE "%2009%"
+        GROUP BY e.EmployeeId)
+
